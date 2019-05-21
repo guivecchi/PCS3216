45520a757c8c92350317ae0f0108c6c216373217
Guilherme Vecchi <guilhermebvecchi@hotmail.com>
2019-05-21 16:54:14 -0300

    Adiciona arquivo memory.py
    Adiciona arquivo responsável pelas operações sobre a memória a ser simulada.
    Designa uma lista a funcionar como a memória simulada e declara funções a serem
    performadas sobre a memória como leitura, escrita e print de bytes e words.
    Impede que essas operações sejam feitas sobre endereços inválidos.

---
+++ src/memory/memory.py
@@ -0,0 +1,72 @@
+MEMORY_SIZE = 65563
+
+# memory allocation starts in 0 and goes to 65562
+memory = [None, None, None, None, None, None, None, None, None, None]
+
+
+def read_byte(address):
+    if(address < 0 or address > 65562):
+        return "UNABLE TO ACCESS ADRESS"
+    else:
+        return memory[address]
+
+
+def print_byte(address):
+    if(address < 0 or address > 65562):
+        print("UNABLE TO ACCESS ADRESS")
+    else:
+        print(memory[address])
+
+
+def write_byte(value, address):
+    if(address < 0 or address > 65562):
+        print "UNABLE TO ACCESS ADRESS"
+    elif(value > 0xff):
+        print("THE VALUE IS BIGGER THAN A BYTE")
+        return 0
+    else:
+        memory[address] = hex(value)
+
+
+def read_word(address):
+    if(address < 0 or address > 65561):
+        return "UNABLE TO ACCESS ADRESS"
+    elif(str(memory[address + 1]) == "None"):
+        return (str(memory[address]) + str(memory[address + 1]))
+    else:
+        return (str(memory[address]) + str(memory[address + 1])[2:])
+
+
+def print_word(address):
+    if(address < 0 or address > 65561):
+        print("UNABLE TO ACCESS ADRESS")
+    elif(str(memory[address + 1]) == "None"):
+        print(str(memory[address]) + str(memory[address + 1]))
+    else:
+        print(str(memory[address]) + str(memory[address + 1])[2:])
+
+
+def write_word(value, address):
+    if(address < 0 or address > 65561):
+        return "UNABLE TO ACCESS ADDRESS"
+    else:
+        if(value < 0x100):
+            memory[address] = hex(0x00)
+            memory[address + 1] = hex(value)
+        elif(value < 0x1000):
+            memory[address] = hex(int("0" + str(hex(value))[2]))
+            memory[address +
+                   1] = hex(int(str(hex(value))[3] + str(hex(value))[4], 16))
+        elif(value > 0xffff):
+            print("THE VALUE IS BIGGER THAN A WORD")
+            return 0
+        else:
+            memory[address] = hex(
+                int(str(hex(value))[2] + str(hex(value))[3], 16))
+            memory[address +
+                   1] = hex(int(str(hex(value))[4] + str(hex(value))[5], 16))
+
+
+print('\n')
+for i in range(0, 10):
+    print("Address " + str(i) + ": " + str(memory[i]))
