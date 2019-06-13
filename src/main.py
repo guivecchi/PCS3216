6cca3b52d8704fe002d50c106979ee74c4461604
Guilherme Vecchi <guilhermebvecchi@hotmail.com>
2019-06-12 22:08:56 -0300

    Add main file
    Main file will be the main script to run

---
+++ src/main.py
@@ -0,0 +1,7 @@
+# Script principal do motor de eventos
+
+print("Bem-vindo ao simulador de eventos")
+
+while(True): # Loop Infinito
+    input_text = input("> ") # Pega comando do usuário
+    print(input_text)
